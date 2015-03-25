0eaf864dba78ad39fdcfd39a966df40b35ab379b
Patrick <psinco@fas.harvard.edu>
2015-03-25 04:42:33 -0500

    change name of result class; minor changes

--- main.php
+++ main.php
@@ -1,11 +1,11 @@
 <?php
 require_once 'inc/class-game-state.php';
-require_once 'inc/class-result-query.php';
+require_once 'inc/class-result.php';
 
 $game_state = new GameState();
 
 $game_state->outs = 0;
-$game_state->inning = '3';
+$game_state->inning = 3;
 $game_state->half = 'top';
 $game_state->bases = 5;
 $game_state->zero_outs = true;
@@ -15,7 +15,7 @@ $game_state->away_team_runs = 2;
 $game_state->p_sym = 'x';
 $game_state->against = 'b';
 $game_state->infield = 'c' ;
-$game_state->fielding = '2';
+$game_state->fielding = 2;
 $game_state->double_cols = false;
 $game_state->star14 = false;
 $game_state->batter_speed = 'n';
@@ -24,7 +24,12 @@ $game_state->on_2b_speed = null;
 $game_state->on_3b_speed = 'f';
 $game_state->play_it_safe = null;
 
-$result_query = new ResultQuery( '12', $game_state );
-echo $result_query;
+
+// get a random value to look up
+$possibles = array( 1, 9, 12, 14, 21, 29, 37 );
+$val = (string) rand( 0, count( $possibles ) - 1 );
+
+$result = new Result( $possibles[$val], $game_state );
+echo $result . PHP_EOL;
 
 ?>
