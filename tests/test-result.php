8b74e25cae3ab6311b15ca5575b7cc70da23eca0
Patrick <psinco@fas.harvard.edu>
2015-03-28 04:52:57 -0500

    scaffold tests

---
+++ tests/test-result.php
@@ -0,0 +1,14 @@
+<?php
+
+class Test_Result extends PHPUnit_Framework_TestCase
+{
+
+  public function test_true() {
+    $this->assertTrue( false );
+  }
+
+  public function test_new_result_object_is_created() {
+  
+  }
+
+} // eoc
